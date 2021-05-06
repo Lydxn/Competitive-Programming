896892c31e88673ed0af2faf8a498c9a60e50eff
Dingledooper <ho3.14159@gmail.com>
2021-05-06 15:41:36 -0700

    Re-add sac21

---
+++ DMOJ/sac21p5.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<ll, int> plli;
+typedef pair<int, ll> pill;
+typedef pair<pair<int, int>, int> piii;
+
+const int INF = 0x3f3f3f3f;
+const ll INFL = 0x3f3f3f3f3f3f3f3fLL;
+const int MOD = 1e9 + 7;
+
+template <class T, class C = less<T>>
+using ordered_set = tree<T, null_type, C, rb_tree_tag, tree_order_statistics_node_update>;
+
+template <const int MAXN, const int INDEXING>
+struct DSU {
+	int par[MAXN + INDEXING], sz[MAXN + INDEXING];
+	void init(int N = MAXN) { for (int i = 0; i < N + INDEXING; i++) par[i] = i, sz[i] = 1; }
+	int find(int v) { return v == par[v] ? v : par[v] = find(par[v]); }
+	bool join(int u, int v) {
+		if ((u = find(u)) == (v = find(v))) return false;
+		if (sz[u] < sz[v]) std::swap(u, v);
+		par[v] = u, sz[u] += sz[v];
+		return true;
+	}
+	bool connected(int u, int v) { return find(u) == find(v); }
+	int size(int v) { return sz[find(v)]; }
+};
+
+DSU<100000, 1> dsu;
+
+int main() {
+	cin.sync_with_stdio(0);
+	cin.tie(0);
+
+	int N, Q, op, u, v;
+	cin >> N >> Q; dsu.init(N);
+	while (Q--) {
+		cin >> op;
+		if (op == 1) cin >> u >> v, dsu.join(u, v);
+		else cin >> u, cout << dsu.sz[dsu.find(u)] << '\n';
+	}
+	return 0;
+}
